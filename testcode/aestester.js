2c42ae43ecde8cf8dba2b437327920bf91436e82
Aziz Faozi <admin@labseni.com>
2019-10-16 17:39:54 +0700

    nambahin tester

---
+++ testcode/aestester.js
@@ -0,0 +1,55 @@
+var aesjs = require('aes-js');
+
+// An example 128 bit key
+
+var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+
+// 128-bit, 192-bit and 256-bit keys
+var key_128 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var key_192 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+               16, 17, 18, 19, 20, 21, 22, 23];
+var key_256 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+               16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+               29, 30, 31];
+ 
+// or, you may use Uint8Array:
+var key_128_array = new Uint8Array(key_128);
+var key_192_array = new Uint8Array(key_192);
+var key_256_array = new Uint8Array(key_256);
+ 
+// or, you may use Buffer in node.js:
+var key_128_buffer = Buffer.from(key_128);
+var key_192_buffer = Buffer.from(key_192);
+var key_256_buffer = Buffer.from(key_256);
+
+
+// An example 128-bit key (16 bytes * 8 bits/byte = 128 bits)
+var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
+ 
+// Convert text to bytes
+var text = 'tanjngerlqwkqlwkqlwkqlwkqlwkl';
+var textBytes = aesjs.utils.utf8.toBytes(text);
+ 
+// The counter is optional, and if omitted will begin at 1
+var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+var encryptedBytes = aesCtr.encrypt(textBytes);
+ 
+// To print or store the binary data, you may convert it to hex
+var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+console.log(encryptedHex);
+// "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
+//  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
+ 
+// When ready to decrypt the hex string, convert it back to bytes
+var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+ 
+// The counter mode of operation maintains internal state, so to
+// decrypt a new instance must be instantiated.
+var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+ 
+// Convert our bytes back into text
+var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+console.log(decryptedText);
+// "Text may be any length you wish, no padding is required."
---
+++ testcode/test1.js
@@ -0,0 +1,9 @@
+const uuidv1 = require('uuid/v1')
+const uuidv4 = require('uuid/v4')
+
+var uuid1 = uuidv1()
+
+console.log(uuid1);
+
+
+console.log("anjing")
