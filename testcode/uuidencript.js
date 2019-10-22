7436cde1778f3b371c5a74328a90b8401b8eb2e6
Aziz Faozi <admin@labseni.com>
2019-10-22 09:52:59 +0700

    Ahzek

--- testcode/uuidencript.js
+++ testcode/uuidencript.js
@@ -42,6 +42,7 @@ var encryptedBytes = aesCtr.encrypt(textBytes);
 // To print or store the binary data, you may convert it to hex
 var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
 console.log(encryptedHex);
+console.log(typeof encryptedHex)
 // "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
 //  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
  
