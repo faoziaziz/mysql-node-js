ed2c6d5f8f1125425ad21f2cdbaff94147e0b312
Aziz Faozi <admin@labseni.com>
2019-11-13 16:41:17 +0700

    modive encripsi

--- testcode/aestest2.js
+++ testcode/aestest2.js
@@ -1,5 +1,5 @@
 var aesjs = require('aes-js');
-
+var teksman= "cieaerqwqw"
 // An example 128 bit key
 
 var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
@@ -31,30 +31,8 @@ var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
 
 var text = 'tanjngerlqwkqlwkqlwkqlwkqlwkl';
 var textBytes = aesjs.utils.utf8.toBytes(text);
- 
-// The counter is optional, and if omitted will begin at 1
-var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-var encryptedBytes = aesCtr.encrypt(textBytes);
-
-
-// To print or store the binary data, you may convert it to hex
-var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-//console.log(typeof encryptedHex);
-// "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
-//  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
-console.log(encryptedHex); 
-// When ready to decrypt the hex string, convert it back to bytes
-var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
-
-console.log("enkripsi bytes")
-console.log(encryptedBytes);
-//console.log(encryptedBytes)
-// The counter mode of operation maintains internal state, so to
-// decrypt a new instance must be instantiated.
-var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-var decryptedBytes = aesCtr.decrypt(encryptedBytes);
- 
-// Convert our bytes back into text
-var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+var string = new TextDecoder("utf-8").decode(textBytes);
+var decryptedText = aesjs.utils.utf8.fromBytes(textBytes);
+console.log("isro1")
+console.log(textBytes);
 console.log(decryptedText);
-// "Text may be any length you wish, no padding is required."
